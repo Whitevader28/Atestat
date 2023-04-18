03c3c11c8ea3e7cebcaa3ed4d2dbdfe723f3e9ea
Whitevader28 <109684620+Whitevader28@users.noreply.github.com>
2023-04-18 17:24:51 +0000

    Text format

--- atestsat/scripts/navbar.js
+++ atestsat/scripts/navbar.js
@@ -5,8 +5,8 @@ var emptyVar = document.body.scrollTop;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-    // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
-    // Add this if you want it to disapear when you scroll all the way up
+        // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+        // Add this if you want it to disapear when you scroll all the way up
         document.getElementById("navbar").style.top = "0";
     }
     else {
