63949c438a66c95e2d8c36ec67dfaf161f2580c7
Whitevader28 <109684620+Whitevader28@users.noreply.github.com>
2023-04-18 16:17:16 +0000

    Created second page

---
+++ atestsat/scripts/navbar.js
@@ -0,0 +1,16 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+
+var prevScrollpos = window.pageYOffset;
+var emptyVar = document.body.scrollTop;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+    // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    // Add this if you want it to disapear when you scroll all the way up
+        document.getElementById("navbar").style.top = "0";
+    }
+    else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
